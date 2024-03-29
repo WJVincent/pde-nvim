b36c4fb9d3e122e8ad65f171c51874916cd52c4d
William Vincent <william@william-vincent.dev>
2024-03-29 18:15:30 -0400

    added telescope plugins, theme, status bar, and which key

--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -13,7 +13,67 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- list of plugins
-local lazy_plugins = {}
+local lazy_plugins = {
+  
+  -- fuzzy finder (files, text, lsp, ect)
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
+  },
+
+  -- file browser using telescope
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+  },
+  
+  -- display pending keybinds
+  { 
+    'folke/which-key.nvim',  
+    opts = {} 
+  },
+
+  -- color theme
+  {
+    'Mofiqul/dracula.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'dracula'
+    end,
+  },
+  
+  -- highlighting and base for language parsing
+  {
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+  },
+
+  -- status line
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'dracula',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+}
 
 -- any option changes from the defualt
 local lazy_opts = {}
