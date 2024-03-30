a7fab51d2df79611e8e969e649c7a88685c437a4
William Vincent <william@william-vincent.dev>
2024-03-30 03:11:02 -0400

    highlight on yank

--- init.lua
+++ init.lua
@@ -12,3 +12,12 @@ require("lazy-setup")
 require("telescope-setup")
 require("options")
 require("keymaps")
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
