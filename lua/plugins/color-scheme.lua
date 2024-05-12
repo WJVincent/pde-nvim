8f49fb0942fb38d4f14be90dff61310aadf74950
William Vincent <william@william-vincent.dev>
2024-05-12 15:35:01 -0400

    gruvbox theme

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -1,25 +1,8 @@
 return {
-	"Mofiqul/dracula.nvim",
+	"ellisonleao/gruvbox.nvim",
 	priority = 1000,
 	config = function()
-		local dracula = require("dracula")
-		local colors = require("dracula").colors()
-		dracula.setup({
-			overrides = {
-				NormalFloat = {
-					fg = colors.fg,
-					bg = colors.selection,
-				},
-			},
-		})
-		vim.cmd.colorscheme("dracula")
+		vim.o.background = 'dark'
+		vim.cmd.colorscheme("gruvbox")
 	end,
 }
--- return {
--- 	"ellisonleao/gruvbox.nvim",
--- 	priority = 1000,
--- 	config = function()
--- 		vim.o.background = 'dark'
--- 		vim.cmd.colorscheme("gruvbox")
--- 	end,
--- }
